8c19a76e84649f41b9c295267f60440b69517053
Julius Ramphabana <jurampha@c4r16s10.wethinkcode.co.za>
2020-07-14 10:37:29 +0200

    displays a string of characters on the standard output

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,12 @@
+void ft_putchar(char c){
+    write(1, &c, 1);
+}
+
+void ft_putstr(char *str){
+
+  while(*str != '\0'){
+      ft_putchar(*str);
+      ft_putchar('\n');
+      str =str + 1;
+  }
+}    
