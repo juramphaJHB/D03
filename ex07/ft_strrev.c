345d0c4de8558fd5b7287dfd4b89de01d68b2e0d
Julius Ramphabana <jurampha@c4r16s10.wethinkcode.co.za>
2020-07-14 10:39:09 +0200

    reverses the order of characters in a string and return str

---
+++ ex07/ft_strrev.c
@@ -0,0 +1,24 @@
+void ft_putchar(char c){
+    write(1, &c, 1);
+}
+
+ char ft_strlen(char *str){
+ int count = 0;
+ char arr[count];
+  while(*str != '\0'){
+      arr[count] = *str;
+      str = str + 1;
+      count++;
+      
+  }
+
+  int count2 = count;
+  while(count2 > -1 ){
+
+    *str = arr[count2];
+    count2--;
+  }
+
+  return str;
+}    
+
