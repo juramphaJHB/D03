065a18dfee06a4811e9c810c6aa97c844d8d2dca
Julius Ramphabana <jurampha@c4r16s10.wethinkcode.co.za>
2020-07-14 10:46:00 +0200

    counts and returns the number of characters in a string.

--- ex06/ft_strlen.c
+++ ex06/ft_strlen.c
@@ -1,16 +1,9 @@
-#include <unistd.h>
-
-void ft_putchar(char c){
-    write(1, &c, 1);
-}
-
- int ft_strlen(char *str){
- int count = 0;
+int ft_strlen(char *str){
+int count = 0;
   while(*str != '\0'){
-      str = str + 1;
-      count++;
+    str = str + 1;
+    count++;
   }
-  ft_putchar(count + '0');
-  return (count);
+return (count);
 }    
 
